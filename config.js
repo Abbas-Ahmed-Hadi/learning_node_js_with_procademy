338057815108b7d9147fe5458193352613a21c2e
Abbas-Ahmed-Hadi <abbas.ahmed.hadi.alzamili@gmail.com>
2026-09-14 10:00:48 +0000

    Comolete Lesson 64

--- config.js
+++ config.js
@@ -2,12 +2,19 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "./config.env" });
 
-const PORT = process.env.PORT ?? 3000;
+const SERVER_PORT = process.env.SERVER_PORT ?? 3000;
 const HOST_NAME = process.env.HOST_NAME ?? "127.0.0.1";
 const NODE_ENV = process.env.NODE_ENV ?? "prodection";
 
+const MONGODB_CONN_STR = process.env.MONGODB_CONN_STR ?? "mongodb://localhost:27017";
+const DB_NAME = process.env.DB_NAME ?? "MyFirstDB";
+
+const DB_CONN_STR = //new URL(MONGODB_CONN_STR, DB_NAME);
+    MONGODB_CONN_STR + '/' + DB_NAME;
+
 export {
-    PORT,
+    SERVER_PORT,
     HOST_NAME,
-    NODE_ENV
+    NODE_ENV,
+    DB_CONN_STR
 }
